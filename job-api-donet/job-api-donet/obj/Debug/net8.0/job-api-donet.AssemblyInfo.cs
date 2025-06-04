36ab22bca9cfb1e58df7416478731be068484fdb
wli806 <wli806@aucklanduni.ac.nz>
2025-06-04 23:27:15 +1200

    Add wwwroot

--- job-api-donet/job-api-donet/obj/Debug/net8.0/job-api-donet.AssemblyInfo.cs
+++ job-api-donet/job-api-donet/obj/Debug/net8.0/job-api-donet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("job-api-donet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9eb13c9864723ec84db813efe7e671d9ffbede3")]
 [assembly: System.Reflection.AssemblyProductAttribute("job-api-donet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("job-api-donet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- jobi-web/next.config.js
+++ jobi-web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        API_BASE_URL: 'http://localhost:9001',
+        API_BASE_URL: 'http://localhost:5232',
         FRONT_END_URL: 'http://localhost:3000'
 
         // API_BASE_URL: 'https://api.kiwicareer.co.nz',
